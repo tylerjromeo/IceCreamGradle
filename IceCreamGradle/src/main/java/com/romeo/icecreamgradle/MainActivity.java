5d5de5dea98122c6269135a466e00ba11ecc9b8c
Tyler Romeo <tyler.romeo@willowtreeapps.com>
2013-10-05 17:53:50 -0400

    Initial commit

---
+++ IceCreamGradle/src/main/java/com/romeo/icecreamgradle/MainActivity.java
@@ -0,0 +1,33 @@
+package com.romeo.icecreamgradle;
+
+import com.romeo.icecreamgradle.flavor.FlavorActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showFlavor(View view){
+        Intent i = new Intent(this, FlavorActivity.class);
+        startActivity(i);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
